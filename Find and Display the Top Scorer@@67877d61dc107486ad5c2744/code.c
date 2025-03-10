511f377ca5b815b8855e5c3a09b6892e9a6cfb0d
ROGER277-alt <rishikhudania923@gmail.com>
2025-03-10 19:29:21 +0530

    Solution to Find and Display the Top Scorer

--- Find and Display the Top Scorer@@67877d61dc107486ad5c2744/code.c
+++ Find and Display the Top Scorer@@67877d61dc107486ad5c2744/code.c
@@ -9,7 +9,7 @@ int main()
 {
     int N;
     scanf("No of students: %d",N);
-    for(int i=0;i<n;i++){
+    for(int i=0;i<=n;i++){
         scanf("%d %d %d",&students[i].rollno, students[i]name ,students[i]marks);
         if(i==0|| students[i]> topscorer.marks){topscorer = students[i];}
     }
