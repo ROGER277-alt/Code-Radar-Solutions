36d82f9362e1a6321bcb952db6aaf6b7407cb1df
ROGER277-alt <rishikhudania923@gmail.com>
2025-04-09 12:55:27 +0530

    Solution to Check if Array is Sorted

--- Check if Array is Sorted@@676c02002dbc8c3e7a280b6f/code.c
+++ Check if Array is Sorted@@676c02002dbc8c3e7a280b6f/code.c
@@ -1,36 +1,22 @@
-#include <stdio.h>
-#include <stdbool.h>
-
-// Function to check if the array is sorted in ascending order
-bool isArraySorted(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        if (arr[i] > arr[i + 1]) {
-            return false;  // If any element is greater than the next, it's not sorted
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int sorted=1;
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            if(arr[i]>arr[j]){
+                sorted=0;
+            }
         }
     }
-    return true;  // If no elements are out of order, the array is sorted
-}
-
-// Function to print the array
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
-
-int main() {
-    int arr[] = {1, 2, 3, 4, 5};  // Example sorted array
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    printArray(arr, n);
-
-    // Check if the array is sorted
-    if (isArraySorted(arr, n)) {
+    if(sorted){
         printf("Sorted");
-    } else {
+    }else{
         printf("Not Sorted");
     }
-
-    return 0;
 }
