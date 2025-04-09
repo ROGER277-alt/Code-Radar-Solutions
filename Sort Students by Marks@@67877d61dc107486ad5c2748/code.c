edb464e4d8af9b45125b4f787be151a3bad17c02
ROGER277-alt <rishikhudania923@gmail.com>
2025-04-09 18:12:15 +0530

    Solution to Sort Students by Marks

--- Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
+++ Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
@@ -1,48 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-// Define a structure to hold student information
-struct Student {
+#include<stdio.h>
+struct student{
+    int rollNumber;
     char name[50];
-    int marks;
+    float marks;
 };
-
-// Comparison function for sorting based on marks
-int compareMarks(const void *a, const void *b) {
-    // Cast the void pointers to the Student pointers
-    struct Student *studentA = (struct Student *)a;
-    struct Student *studentB = (struct Student *)b;
-    
-    // Return the comparison result (ascending order)
-    return studentA->marks - studentB->marks;
-}
-
-int main() {
+int main(){
     int n;
-
-    // Ask the user for the number of students
-    printf("Enter the number of students: ");
-    scanf("%d", &n);
-
-    struct Student students[n];
-
-    // Input student data (name and marks)
-    for (int i = 0; i < n; i++) {
-        printf("\nEnter the name of student %d: ", i + 1);
-        scanf("%s", students[i].name);
-        printf("Enter the marks of student %d: ", i + 1);
-        scanf("%d", &students[i].marks);
+    scanf("%d",&n);
+    struct student students[n];
+  
+    
+    for(int i=0;i<n;i++){
+        scanf("%d %s %f",&students[i].rollNumber,&students[i].name,&students[i].marks);
     }
-
-    // Sort the students by marks using qsort
-    qsort(students, n, sizeof(struct Student), compareMarks);
-
-    // Print the sorted list of students
-    printf("\nSorted list of students by marks:\n");
-    for (int i = 0; i < n; i++) {
-        printf("%s: %d\n", students[i].name, students[i].marks);
+      for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(students[j].marks < students[j + 1].marks) {
+                struct student temp = students[j];
+                students[j] = students[j + 1];
+                students[j + 1] = temp;
+            }
+        }
     }
-
-    return 0;
-}
+    for(int i=0;i<n;i++){
+    printf("Roll Number: %d, Name: %s, Marks: %.2f\n",students[i].rollNumber,students[i].name,students[i].marks);
+}}
