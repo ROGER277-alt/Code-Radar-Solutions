c1dd5b9fc2036a3b44cfcc1d7a6dc9901b86ef3c
ROGER277-alt <rishikhudania923@gmail.com>
2025-04-09 12:52:51 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -1,37 +1,24 @@
-#include <stdio.h>
-#include <stdbool.h>
-
-// Function to check if the array is sorted in ascending order
-bool isArraySorted(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        if (arr[i] > arr[i + 1]) {
-            return false;  // If any element is greater than the next, it's not sorted
-        }
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n],temp[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
     }
-    return true;  // If no elements are out of order, the array is sorted
-}
-
-// Function to print the array
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
-
-int main() {
-    int arr[] = {1, 2, 3, 4, 5};  // Example sorted array
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    printf("Array: ");
-    printArray(arr, n);
-
-    // Check if the array is sorted
-    if (isArraySorted(arr, n)) {
-        printf("The array is sorted in ascending order.\n");
-    } else {
-        printf("The array is not sorted in ascending order.\n");
+    int a;
+    scanf("%d",&a);
+    a=a%n;
+    
+    for (int i = 0; i < a; i++) {
+        temp[i] = arr[n - a + i];  
     }
 
-    return 0;
+    for (int i = a; i < n; i++) {
+        temp[i] = arr[i - a];  
+    }
+
+    for(int i=0;i<n;i++){
+        printf("%d\n",temp[i]);
+    }
 }
