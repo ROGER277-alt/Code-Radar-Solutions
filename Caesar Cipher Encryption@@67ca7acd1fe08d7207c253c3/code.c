b63a92115298fae380384896d86148b820a64a88
ROGER277-alt <rishikhudania923@gmail.com>
2025-04-09 19:13:21 +0530

    Solution to Caesar Cipher Encryption

--- Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -1,8 +1,17 @@
-// Your code here...
-#include<stdio.h>
-#include<string.h>
-char *caesarCipher(char str[],int n,char encrypted[]){
-    for(int i=0;i<strlen(str);i++){
-        encrypted[i]=str[i]+n;
+#include <stdio.h>
+
+int main() {
+    char text[100];
+    int shift;
+    scanf("%s", text);
+    scanf("%d", &shift);
+
+    for (int i = 0; text[i] != '\0'; i++) {
+        if (text[i] >= 'A' && text[i] <= 'Z') {
+            text[i] = ((text[i] - 'A' + shift) % 26) + 'A';
+        }
     }
-   return encrypted;}
+
+    printf("Encrypted: %s\n", text);
+    return 0;
+}
